7a869245826ec05fdf69efee5fc711c72cdf37b7
Daniela Schwarz <danigoestoca@gmail.com>
2021-08-13 00:19:37 -0400

    Created helper components

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "../style.css";
+import { Link } from "react-router-dom";
 const Navbar = (props) => {
   return (
     <nav aria-label="Primary" class="navigation">
       <ul class="menu">
-        <li class="menu-li">
-            Home
-        </li>
-        <li class="menu-li">
-            Products
-        </li>
-        <li class="menu-li">
-            About
-        </li>
-        <li class="menu-li">
-            Contact
-        </li>
+        <Link to={`/`}>
+          <li className="menu-link">Home</li>
+        </Link>
+        <Link to={`/favorites`}>
+          <li className="menu-link">Favorites</li>
+        </Link>
+        <Link to={`/cart`}>
+          <li className="menu-link">Cart</li>
+        </Link>
+        <li className="menu-link">Contact</li>
       </ul>
     </nav>
   );
